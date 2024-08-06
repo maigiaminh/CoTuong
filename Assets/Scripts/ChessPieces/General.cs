2a432412b92f415eae1f40a7c2e4ee06bcb0cf07
maigiaminh <minh.mgia@gmail.com>
2024-08-06 20:07:16 +0700

    General Pieces Movement

--- Assets/Scripts/ChessPieces/General.cs
+++ Assets/Scripts/ChessPieces/General.cs
@@ -1,4 +1,78 @@
+using System.Collections.Generic;
+using UnityEngine;
+
 public class General : ChessPiece
 {
-    
+    public override List<Vector2Int> GetAvailableMoves(ref ChessPiece[,] board, int boardX, int boardY){
+        List<Vector2Int> moves = new List<Vector2Int>();
+        /** MOVEMENT **/
+        
+        /*UP*/
+        if((currentY + 1 <= 2 && team == 0) || (currentY + 1 < boardY && team == 1)){
+            //Posible Moves
+            if(board[currentX, currentY + 1] == null){
+                moves.Add(new Vector2Int(currentX, currentY + 1));
+            }
+
+            //Possible Kill Moves
+            if(board[currentX, currentY + 1] != null){
+                if(board[currentX, currentY + 1].team != team){
+                    moves.Add(new Vector2Int(currentX, currentY + 1));
+                }
+                
+            }
+
+        }
+
+        /*DOWN*/
+        if((currentY - 1 >= 0 && team == 0) || (currentY - 1 >= 7 && team == 1)){
+            //Posible Moves
+            if(board[currentX, currentY - 1] == null){
+                moves.Add(new Vector2Int(currentX, currentY - 1));
+            }
+
+            //Possible Kill Moves
+            if(board[currentX, currentY - 1] != null){
+                if(board[currentX, currentY - 1].team != team){
+                    moves.Add(new Vector2Int(currentX, currentY - 1));
+                }
+                
+            }
+
+        }
+
+        /*LEFT*/
+        if(currentX - 1 >= 3){
+            //Posible Moves
+            if(board[currentX - 1, currentY] == null){
+                moves.Add(new Vector2Int(currentX - 1, currentY));
+            }
+
+            //Possible Kill Moves
+            if(board[currentX - 1, currentY] != null){
+                if(board[currentX - 1, currentY].team != team){
+                    moves.Add(new Vector2Int(currentX - 1, currentY));
+                }
+                
+            }
+
+        }
+
+        /*RIGHT*/
+        if(currentX + 1 <= 5){
+            //Posible Moves
+            if(board[currentX + 1, currentY] == null){
+                moves.Add(new Vector2Int(currentX + 1, currentY));
+            }
+
+            //Possible Kill Moves
+            if(board[currentX + 1, currentY] != null){
+                if(board[currentX + 1, currentY].team != team){
+                    moves.Add(new Vector2Int(currentX + 1, currentY));
+                }
+            }
+        }
+
+        return moves;
+    }
 }
--- Assets/Scripts/ChessPieces/Soldier.cs
+++ Assets/Scripts/ChessPieces/Soldier.cs
@@ -5,9 +5,9 @@ public class Soldier : ChessPiece
 {
     public override List<Vector2Int> GetAvailableMoves(ref ChessPiece[,] board, int boardX, int boardY){
         List<Vector2Int> moves = new List<Vector2Int>();
-        
         int direction = (team == 0) ? 1 : -1;
-
+        
+        /** MOVEMENT **/
         if(currentY + direction > 0 && currentY + direction < boardY){
             //Possible Moves
             if(board[currentX, currentY + direction] == null){
